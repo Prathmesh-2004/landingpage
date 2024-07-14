43cc97d8062a6f1968b9b04709d3f3f6f2596328
Prathmesh-2004 <02prathmeshbendre@gmail.com>
2024-07-14 18:03:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const buyNowButton = document.getElementById('buy-now');
+
+buyNowButton.addEventListener('click', () => {
+	alert('Thank you for purchasing FitMax!');
+	// Add functionality to handle the purchase process
+});
